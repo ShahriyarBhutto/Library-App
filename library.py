b5c72815b05b621c6567479a00f8c5f1a336e367
Shahriyar Bhutto <88304298+ShahriyarBhutto@users.noreply.github.com>
2025-07-03 16:16:41 +0500

    Update library.py
    Some Validations and Book adding functionality is implemented

--- library.py
+++ library.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel, Field
 
 
 
@@ -7,17 +8,41 @@ app = FastAPI()
 
 class Library:
     book_id: int
-    book_tilte: str
+    book_title: str
     book_author: str
     book_rating: int
     book_publish_year: int
 
     def __init__(self, book_id, book_title, book_author, book_rating, book_publish_year):
         self.book_id = book_id
-        self.book_tilte = book_title
+        self.book_title = book_title
         self.book_author = book_author
         self.book_rating = book_rating
         self.book_publish_year = book_publish_year
+
+
+class BookRequest(BaseModel):
+    book_id: int = Field(gt=0)
+    book_title: str = Field(min_length= 3)
+    book_author: str = Field(min_length= 3)
+    book_rating: int = Field(gt=0, lt=6)
+    book_publish_year: int = Field(gt= 1900, lt= 2030)
+
+    model_config = {
+        "json_schema_extra":{
+            "example":{
+                "book_id":1,
+                "book_title":"title-abc",
+                "book_author":"author-abc",
+                "book_rating":4,
+                "book_publish_year":1995,
+
+
+            }
+        }
+    }
+
+
 
 
 LIBRARY = [
@@ -38,13 +63,13 @@ async def home():
 
 # Show All Books:
 
-@app.get("/books")
+@app.get("/books", status_code= status.HTTP_200_OK)
 async def get_all_books():
     return LIBRARY
 
 # Search Books By Their Id:
 
-@app.get("/books/{book_id}")
+@app.get("/books/{book_id}" , status_code= status.HTTP_200_OK)
 async def get_book_by_id(book_id: int):
     for book in LIBRARY:
         if book.book_id == book_id:
@@ -52,10 +77,19 @@ async def get_book_by_id(book_id: int):
 
 # Sort Books According to Publish Year
 
-@app.get("/books/")
+@app.get("/books/", status_code= status.HTTP_200_OK)
 async def sort_books_by_year(book_year: int):
     books_to_return = []
     for book in LIBRARY:
         if book.book_publish_year == book_year:
             books_to_return.append(book)
     return books_to_return    
+
+
+# Add New Books in the Library:
+
+@app.post("/books/create_book", status_code = status.HTTP_201_CREATED )
+async def add_new_book(request_book: BookRequest):
+    new_book = Library(**request_book.model_dump())
+    LIBRARY.append(new_book)
+
